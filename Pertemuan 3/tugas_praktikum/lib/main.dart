523b80aabec61e7eb300ab5d7cbf92e817e241fd
Yusufa Haidar <91399445+PencuriHandal@users.noreply.github.com>
2023-09-12 09:21:03 +0700

    Update Style Font dan Theme

--- Pertemuan 3/tugas_praktikum/lib/main.dart
+++ Pertemuan 3/tugas_praktikum/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red),
+        scaffoldBackgroundColor: Colors.black,
+      ),
       home: Scaffold (
         appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text("Tugas Praktikum"),
+          centerTitle: true,
+          title: Image.network("https://liquipedia.net/commons/images/thumb/5/51/VCT_icon_lightmode.png/900px-VCT_icon_lightmode.png",
+          height: 50),
         ),
         body: ListView(
           children: <Widget>[
@@ -22,8 +28,8 @@ class MyApp extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text("BERITA TERBARU", style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text("ESPORT NEWS HARI INI", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("BERITA TERBARU", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))),
+                  Text("ESPORT NEWS HARI INI", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))),
                 ],
               ),
             ),
@@ -34,7 +40,7 @@ class MyApp extends StatelessWidget {
                 fit: BoxFit.cover,
                 ),
                 border: Border.all(
-                  color: Colors.purpleAccent,
+                  color: Colors.red,
                   width: 0.5,
                 ),
               ),
@@ -44,12 +50,13 @@ class MyApp extends StatelessWidget {
             ),
             Container(
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.purpleAccent)
+                border: Border.all(color: Colors.redAccent),
+                color: Colors.white
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text("Anything You Need to Know about EG", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("Anything You Need to Know about EG", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold))),
                 ],
               ),
               height: 30,
@@ -57,9 +64,9 @@ class MyApp extends StatelessWidget {
               margin: EdgeInsets.only(left: 20, right: 20),
             ),
             Container(
-              color: Colors.purpleAccent,
+              color: Colors.redAccent,
               alignment: Alignment.centerLeft,
-              child: Text("\tView More ->", style: TextStyle(fontWeight: FontWeight.bold)),
+              child: Text("\tView More ->", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold))),
               height: 30,
               width: 400,
               margin: EdgeInsets.only(left: 20, right: 20),
@@ -72,7 +79,7 @@ class MyApp extends StatelessWidget {
                   height: 100,
                   width: 152.7,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.greenAccent)
+                    border: Border.all(color: Colors.redAccent)
                   ),
                   child: Image.network('https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blt44454a513589fbde/64c29614570512e1c0dd6bff/VCT23_Champions_WPEArticle.jpg?width=2472&height=1390',
                                         fit: BoxFit.cover,
@@ -80,7 +87,8 @@ class MyApp extends StatelessWidget {
                 ),
                 Container(
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.greenAccent)
+                    border: Border.all(color: Colors.redAccent),
+                    color: Colors.white
                   ),
                   margin: const EdgeInsets.only(right: 20),
                   height: 100,
@@ -89,19 +97,20 @@ class MyApp extends StatelessWidget {
                   constraints: BoxConstraints(
                     maxWidth: 200.0,
                   ),
-                child: Text("Watch, Play, and Collect, during VCT 2023", style: TextStyle(fontSize: 12),),
+                child: Text("Watch, Play, and Collect, during VCT  2023", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 12))),
                 ),
               ],
             ),
             Container(
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.greenAccent)
+                border: Border.all(color: Colors.redAccent),
+                color: Colors.white
               ),
               height: 30,
               width: 400,
               margin: EdgeInsets.only(left: 20, right: 20),
               alignment: Alignment.centerLeft,
-              child: Text("\tLos Angeles, Jul 29 2023"),
+              child: Text("\tLos Angeles, Jul 29 2023", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold))),
             ),
             SizedBox(height: 10),
             Row(
@@ -111,7 +120,7 @@ class MyApp extends StatelessWidget {
                   height: 100,
                   width: 152.7,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey)
+                    border: Border.all(color: Colors.redAccent)
                   ),
                   child: Image.network('https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/bltcd97590cc1bb7a83/64d55fd5fe29dd9d4130f4fd/VCT_OffSeason_2022_Header.jpg?width=2472&height=1390',
                                         fit: BoxFit.cover,
@@ -119,7 +128,8 @@ class MyApp extends StatelessWidget {
                 ),
                 Container(
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey)
+                    border: Border.all(color: Colors.redAccent),
+                    color: Colors.white
                   ),
                   margin: const EdgeInsets.only(right: 20),
                   height: 100,
@@ -128,19 +138,20 @@ class MyApp extends StatelessWidget {
                   constraints: BoxConstraints(
                     maxWidth: 200.0,
                   ),
-                child: Text("Announcement VCT//Off Season", style: TextStyle(fontSize: 12),),
+                child: Text("Announcement VCT//Off Season", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 12))),
                 ),
               ],
             ),
             Container(
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey)
+                border: Border.all(color: Colors.redAccent),
+                color: Colors.white
               ),
               height: 30,
               width: 400,
               margin: EdgeInsets.only(left: 20, right: 20),
               alignment: Alignment.centerLeft,
-              child: Text("\tLos Angeles, Aug 11 2023"),
+              child: Text("\tLos Angeles, Aug 11 2023", style: GoogleFonts.montserrat(textStyle: TextStyle(fontWeight: FontWeight.bold))),
             ),
           ],
         ),
