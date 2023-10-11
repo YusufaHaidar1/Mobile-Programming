d3fd4c31845574afc95e17295f67bd3d244f6789
Yusufa Haidar <91399445+PencuriHandal@users.noreply.github.com>
2023-10-11 15:45:24 +0700

    Pengumpulan Pertemuan 7

---
+++ Pertemuan 7/http_request/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:http_request/pages/movie_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context){
+    return  MovieList();
+  }
+}
